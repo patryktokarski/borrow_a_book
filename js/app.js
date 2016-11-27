bfacb7211257ec84b9c9333cbd270c7794824b6b
patryktokarski <patryktok@gmail.com>
2016-11-28 00:10:14 +0100

    optimised code

--- js/app.js
+++ js/app.js
@@ -5,24 +5,36 @@ $(document).ready(function() {
     function loadBooks() {
         $('div.ajaxStatus').toggle();
         $.get(endpoint, function(json) {
-            $('table#ksiazki').find('tr.book').remove();
-            $('table#ksiazki .info').remove();
-            $('table#ksiazki .editForm').remove();
+            $('div#books').find('.book').remove();
+            $('div#books .info').remove();
+            $('div#books .editForm').remove();
             var books = JSON.parse(json);
             
             for (var i = 0; i < books.length; i++) {
-                var book = $('<tr class="book" data-book-id='+ books[i].id + '>\n\
-                                <td class="title">'+ books[i].name +'</td>\n\
-                                <td><a class="remove" href="#">Usun</a></td></tr>');
+                var book = $('<div class="book" data-book-id='+ books[i].id + '><span class="titleSpan">Title: \n\
+                              <span class="title">'+ books[i].name +'</span></span>\n\
+                              <div class="deleteBtn btn"><a class="removeBtn btn" href="#">DELETE</a></div></div>');
                 
-                $('table#ksiazki').append(book);
+                $('div#books').append(book);
                 
                 
-                var informations = $('<div class="info" data-book-id="'+books[i].id+'" style = "display:none">\n\
-                                    <p> '+ books[i].author +'</p><p>'+ books[i].book_desc +'</p>\n\
-                                    <a class="edit" href="#">Edytuj</a></div>');
+                var informations = $('<div class="info" style = "display:none">\n\
+                                    <div class="author"><span class = "author">' + books[i].author +'</span></div>\n\
+                                    <div class="description"><span class = "desc">' + books[i].book_desc + '</span></div>\n\
+                                    <div class="editBtn"><a class="edit btn" href="#">EDIT</a></div></div>');
               
-                book.after(informations);
+                book.append(informations);
+
+                var editForm = $('<div class = "editForm" style = "display: none">\n\
+                                  <p>Formularz edycji</p><hr>\n\
+                                  <label>Title:</label><input name = "name" value ="' + books[i].name + '">\n\
+                                  <label>Author:</label><input name = "author" value ="' + books[i].author + '">\n\
+                                  <label>Description:</label><input name = "book_desc" value ="' + books[i].book_desc + '">\n\
+                                  <div class = "optionBtn"><a class = "saveForm btn" href = "#">SAVE</a>\n\
+                                  <a class = "cancelForm btn" href = "#">CANCEL</a></div>\n\
+                                  </div>');
+
+                informations.append(editForm);
                 
             }
             $('div.ajaxStatus').toggle();
@@ -31,43 +43,22 @@ $(document).ready(function() {
 
     loadBooks();
     
-    $('table#ksiazki').on('click', '.title',function() {
-        var bookId = $(this).parent().attr('data-book-id');
-        $('div[data-book-id="' + bookId + '"]').slideToggle();
-        
+    $('div#books').on('click', '.titleSpan',function() {
+
+        var bookId = $(this).parent().data('book-id');
+        $('[data-book-id=' + bookId + ']').find('div.info').slideToggle();
     });
 
-    $('a#refresh').on('click', function() {
-        loadBooks();
-    });
-    
-    $('table#ksiazki').on('click', '.edit', function() {
-        
-        var name = $(this).parent().prev().find('td.title').text();        
-        var author = $(this).parent().first().find('p').eq(0).text();
-        var book_desc = $(this).parent().first().find('p').eq(1).text();
-        
-        if(!$(this).parent().next().hasClass('editForm')) {
-             var editForm = $('<div class="editForm" data-book-id = "'+ $(this).parent().attr('data-book-id') +'"style="display:none">\n\
-                                 <p>Formularz edycji</p>\n\
-                                 <label>Tytuł:</label><input name="name" value="' + name + '">\n\
-                                 <label>Autor:</label><input name="author" value="' + author + '">\n\
-                                 <label>Opis ksiazki:</label><input name="book_desc" value="' + book_desc + '">\n\
-                                 <a class="saveForm" href="#">Zapisz</a>\n\
-                                 <a class="cancelForm" href="#">Anuluj</a>\n\
-                               </div>');
-             $(this).parent().after(editForm);
-        }
-        $(this).parent().next().slideDown();
-    
-    });
-    
-   
-    $('table#ksiazki').on('click', '.cancel', function() {
-        $('div.editForm').remove();
+    $('div#books').on('click', '.edit', function(e) {
+
+        e.preventDefault();
+        var bookId = $(this).parents('div.book').data('book-id');
+        $('[data-book-id=' + bookId + ']').find('.editForm').slideDown();
+
     });
     
     function updateBook(authorVal, nameVal, book_descVal, book_idVal) {
+
         $('div.ajaxStatus').toggle();
 
 	$.post(endpoint, {
@@ -77,39 +68,42 @@ $(document).ready(function() {
             book_id: book_idVal
         })
         .done(function(json) {
-            console.log(json);
             
-            $('table#ksiazki').find('div.editForm').remove();
-            $('table#ksiazki').find('div.info').remove();
+            $('div#books').find('div.editForm').remove();
+            $('div#books').find('div.info').remove();
 
             loadBooks();
         })
-	.fail(function(xhr) {
-            console.log('update error', xhr);
-	})
-	.always(function(xhr) {
-            $('div.ajaxStatus').toggle();
+        .fail(function(xhr) {
+                console.log('update error', xhr);
+        })
+        .always(function(xhr) {
+                $('div.ajaxStatus').toggle();
         });
     }
     
-    $('table#ksiazki').on('click', 'a.saveForm', function(event) {
-        event.preventDefault();
-        var bookId = $(this).parent().data('book-id');
-	var author = $(this).parents('div.editForm').find('input[name="author"]').prop('value');
-	var name = $(this).parents('div.editForm').find('input[name="name"]').prop('value');
-	var book_desc = $(this).parents('div.editForm').find('input[name="book_desc"]').prop('value');
-	var book_id = $(this).parent().prev().prev().data('book-id');
+    $('div#books').on('click', 'a.saveForm', function(event) {
 
-	updateBook(author, name, book_desc, book_id);
+        event.preventDefault();
+        var bookId = $(this).parents('div.book').data('book-id');
+        var author = $('[data-book-id =' + bookId + ']').find('input[name="author"]').prop('value');
+        var title = $('[data-book-id =' + bookId + ']').find('input[name="name"]').prop('value');
+        var book_desc = $('[data-book-id =' + bookId + ']').find('input[name="book_desc"]').prop('value');
+
+	    updateBook(author, title, book_desc, bookId);
     });    
     
     
-    $('table#ksiazki').on('click', 'a.cancelForm', function() {
-    $(this).parents('div').remove();
+    $('div#books').on('click', 'a.cancelForm', function(e) {
+
+        e.preventDefault();
+        var bookId = $(this).parents('div.book').data('book-id');
+        $('[data-book-id = ' + bookId + ']').find('.editForm').slideUp();
     
     });
 
     function addBook(authorVal, nameVal, book_descVal) {
+
         $('div.ajaxStatus').toggle();
         
         $.ajax({
@@ -132,41 +126,41 @@ $(document).ready(function() {
     }
     
     $('form#addbook').find('input[type="submit"]').click(function() {
-	event.preventDefault();
-	var author = $(this).parent().find('input[name="author"]').prop('value');
-	var name = $(this).parent().find('input[name="name"]').prop('value');
-	var book_desc = $(this).parent().find('input[name="book_desc"]').prop('value');
 
-	console.log(author + name + book_desc);
-	addBook(author, name, book_desc);
+        event.preventDefault();
+        var author = $(this).parent().find('input[name="author"]').prop('value');
+        var name = $(this).parent().find('input[name="name"]').prop('value');
+        var book_desc = $(this).parent().find('input[name="book_desc"]').prop('value');
+
+	    addBook(author, name, book_desc);
     });
     
     function removeBook(bookId) {
-	$('div.ajaxStatus').toggle();
 
-	$.ajax({
-            url: endpoint,
-            data: {book_id: bookId},
-            type: 'DELETE',
-            dataType: 'json'
-	})
-	.done(function(json) {
-            console.log(json);
-            loadBooks();
-	})
-	.fail(function(xhr) {
-            console.log('add error', xhr);
-	})
-	.always(function(xhr) {
-            $('div.ajaxStatus').toggle();
-	})
+        $('div.ajaxStatus').toggle();
+
+        $.ajax({
+                url: endpoint,
+                data: {book_id: bookId},
+                type: 'DELETE',
+                dataType: 'json'
+        })
+        .done(function(json) {
+                loadBooks();
+        })
+        .fail(function(xhr) {
+                console.log('add error', xhr);
+        })
+        .always(function(xhr) {
+                $('div.ajaxStatus').toggle();
+        })
     }
     
-    $('table#ksiazki').on('click', 'a.remove', function() {
-	var bookId = $(this).parents('tr.book').first().data('book-id');
-	if ( confirm('Czy na pewno chcesz usunąć książkę (id:' + bookId + ')') ) {
-            removeBook(bookId);
-	}
+    $('div#books').on('click', 'a.removeBtn', function() {
+        var bookId = $(this).parent().parent().data('book-id');
+        if ( confirm('Do you really want to delete this book: (id:' + bookId + ')') ) {
+                removeBook(bookId);
+        }
     });
 });
 
